90772aed1c44b0f31b1e6dd74d8d1237bb00f7fa
Doc <lastdoc39@gmail.com>
2026-04-08 00:00:58 -0400

    Added initial commit to post-gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,4 +5,6 @@ import os
 if __name__ == "__main__":
     print("Executing 'uv sync'")
     os.system("uv sync")
+    print("Generating initial git commit")
+    os.system('git add . && git commit -m "Initial commit from cookiecutter template"')
     print("Your Python package project has been created successfully!")
